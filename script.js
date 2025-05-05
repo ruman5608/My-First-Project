7d5d0f5d14216eb80557129c536607003d7d954c
Ruman <rumansarwar6688@gmail.com>
2025-05-05 14:23:16 +0530

    First Project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.creative-btn').addEventListener('click', function () {
+    alert('Thank you for choosing to review us!');
+    // Add additional functionality here if needed
+});
